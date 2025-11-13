426ebf8c00e3d1d1a10f02663ac4107fcc72b16c
Lucas Borges da Silva <lucasborges2812@gmail.com>
2025-11-13 15:10:59 -0300

    Criando servidor gRPC

---
+++ cmd/gRPCServer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/devfullcycle/14-gRPC/internal/database"
+	"github.com/devfullcycle/14-gRPC/internal/pb"
+	"github.com/devfullcycle/14-gRPC/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	CategoryService := service.NewCategoryService(*categoryDB)
+
+	//Implementação do servidor gRPC
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, CategoryService)
+	reflection.Register(grpcServer) //Registro do serviço de reflexão, que permite que clientes descubram serviços, métodos e mensagens em tempo de execução. Necessário!!
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
